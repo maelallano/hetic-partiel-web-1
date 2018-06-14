14934d5fbedda63240ce662765e7a61e672289b1
mallano <maelallano1@gmail.com>
2018-06-14 12:50:36 +0200

    Fixes #2

--- js/main.js
+++ js/main.js
@@ -1,7 +1,8 @@
 var burgerMenu = document.querySelector('#burgerMenu');
-var maiNavList = document.querySelector('#mainav ul');
+var maiNavLiscrollTop = document.querySelector('#mainav ul');
 var backtotop = document.querySelector('#backtotop');
 
+//Burger Menu
 burgerMenu.addEventListener('click', function() {
 	if (maiNavList.style.display === "none") {
 		maiNavList.style.display = "block";
@@ -10,9 +11,40 @@ burgerMenu.addEventListener('click', function() {
 	}
 });
 
+//Back to top arrow
 backtotop.addEventListener('click', function() {
 	window.scroll({
 	  top: 0,
 	  behavior: 'smooth'
 	});
 });
+
+
+var headerMenu = document.querySelector('.row1');
+var lastScrollTop = 0;
+
+//Header Menu displayed when scrolling up
+window.addEventListener("scroll", function(){
+   var scrollTop = window.pageYOffset || document.documentElement.scrollTop;
+
+   if (scrollTop > lastScrollTop){
+      headerMenu.style.position = "";
+      headerMenu.style.top = "";
+   } else {
+     headerMenu.style.position = "fixed";
+     headerMenu.style.top = "0";
+   }
+
+   if (scrollTop <= 0) {
+   	lastScrollTop = 0;
+   } else {
+   	lastScrollTop = scrollTop;
+   }
+});
+
+
+
+
+
+
+
